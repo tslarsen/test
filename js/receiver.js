61740b4708f75a6750c9ea4486168f0b3ea69501
tslarsen <mrtlinsd@gmail.com>
2022-06-09 11:59:21 -0700

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -6,16 +6,29 @@ console.log("Hey there");
 if (context.canDisplayType('audio/mp4', ' mhm1.0x0D ')){
   console.log("Supported format: audio/mp4', ' mhm1.0x0D");
 }
+else{
+    console.log("NOT Supported format: audio/mp4', ' mhm1.0x0D");
+}
 
 if (context.canDisplayType('audio/mp4', ' mhm1.0x0E ')){
   console.log("Supported format: audio/mp4', ' mhm1.0x0E");
+}
+else{
+  console.log("NOT Supported format: audio/mp4', ' mhm1.0x0E");
 }
 
 if (context.canDisplayType('audio/mp4', ' mhm1.0x12 ')){
   console.log("Supported format: audio/mp4', ' mhm1.0x12");
 }
+else{
+    console.log("NOT Supported format: audio/mp4', ' mhm1.0x12");
+}
 
 if (context.canDisplayType('audio/mp4; codecs=ec-3; spatialRendering=true')){
   console.log("Supported format: audio/mp4; codecs=ec-3; spatialRendering=true");
 }
+else{
+    console.log("Supported format: audio/mp4; codecs=ec-3; spatialRendering=true");
+}
+
   context.start();
