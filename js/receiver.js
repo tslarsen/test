6406f70a116ea62ac2ced2f8f83614db61e2d13e
tslarsen <mrtlinsd@gmail.com>
2022-06-09 16:18:24 -0700

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,7 +1,6 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
-console.log("Hey there");
 
 
 const ismhm1_0x0D = context.canDisplayType('audio/mp4', ' mhm1.0x0D');
@@ -23,33 +22,5 @@ console.log("Supported format: audio/mp4', ' mha1.0x12: " + ismha1_0x12);
 console.log("Supported format: audio/mp4; codecs=ec-3; spatialRendering=true: " + isatmos);
 console.log("------------------");
 
-
-if (context.canDisplayType('audio/mp4', ' mhm1.0x0D ')){
-  console.log("Supported format: audio/mp4', ' mhm1.0x0D");
-}
-else{
-    console.log("NOT Supported format: audio/mp4', ' mhm1.0x0D");
-}
-
-if (context.canDisplayType('audio/mp4', ' mhm1.0x0E ')){
-  console.log("Supported format: audio/mp4', ' mhm1.0x0E");
-}
-else{
-  console.log("NOT Supported format: audio/mp4', ' mhm1.0x0E");
-}
-
-if (context.canDisplayType('audio/mp4', ' mhm1.0x12 ')){
-  console.log("Supported format: audio/mp4', ' mhm1.0x12");
-}
-else{
-    console.log("NOT Supported format: audio/mp4', ' mhm1.0x12");
-}
-
-if (context.canDisplayType('audio/mp4; codecs=ec-3; spatialRendering=true')){
-  console.log("Supported format: audio/mp4; codecs=ec-3; spatialRendering=true");
-}
-else{
-    console.log("NOT Supported format: audio/mp4; codecs=ec-3; spatialRendering=true");
-}
 
   context.start();
