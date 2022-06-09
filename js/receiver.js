eda36019b8104475a267d07712afca8869222feb
tslarsen <mrtlinsd@gmail.com>
2022-06-09 11:51:36 -0700

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,4 +1,9 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
-context.start();
+console.log("Hey there");
+
+if (context.canDisplayType('audio/mp4', ' mhm1.0x0D ')){
+  console.log("Supported format: audio/mp4', ' mhm1.0x0D");
+}
+  context.start();
