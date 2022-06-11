3b1757bdd590e1eb175e527ec31a41b779ba357c
tslarsen <mrtlinsd@gmail.com>
2022-06-10 17:37:41 -0700

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,7 +1,22 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
+// Debug Logger
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyAPP.LOG';
 
+// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+  if (!castDebugLogger.debugOverlayElement_) {
+      castDebugLogger.setEnabled(true);
+  }
+});
+
+// Set verbosity level for Core events.
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
 
 const ismhm1_0x0D = context.canDisplayType('audio/mp4', ' mhm1.0x0D');
 const ismhm1_0x0E = context.canDisplayType('audio/mp4', ' mhm1.0x0E');
@@ -10,6 +25,8 @@ const ismha1_0x0D = context.canDisplayType('audio/mp4', ' mha1.0x0D');
 const ismha1_0x0E = context.canDisplayType('audio/mp4', ' mha1.0x0E');
 const ismha1_0x12 = context.canDisplayType('audio/mp4', ' mha1.0x12');
 const isatmos = context.canDisplayType('audio/mp4; codecs=ec-3; spatialRendering=true');
+
+castDebugLogger.info(LOG_TAG, 'Tonni Test: Supported format: audio/mp4', ' mhm1.0x0D: ' ismhm1_0x0D);
 
 console.log("------------------");
 
