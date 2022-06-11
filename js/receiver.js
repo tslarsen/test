6932640880615ac7774ec9f37930b59ebd16fa14
tslarsen <mrtlinsd@gmail.com>
2022-06-10 17:46:08 -0700

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -9,7 +9,12 @@ const LOG_TAG = 'MyAPP.LOG';
 context.addEventListener(cast.framework.system.EventType.READY, () => {
   if (!castDebugLogger.debugOverlayElement_) {
       castDebugLogger.setEnabled(true);
-  }
+
+      // Show debug overlay
+      castDebugLogger.showDebugLogs(true);
+
+      // Clear log messages on debug overlay
+      castDebugLogger.clearDebugLogs();  }
 });
 
 // Set verbosity level for Core events.
@@ -17,6 +22,11 @@ castDebugLogger.loggerLevelByEvents = {
   'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
   'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
 }
+
+// Set verbosity level for custom tags.
+castDebugLogger.loggerLevelByTags = {
+    [LOG_TAG]: cast.framework.LoggerLevel.DEBUG,
+};
 
 const ismhm1_0x0D = context.canDisplayType('audio/mp4', ' mhm1.0x0D');
 const ismhm1_0x0E = context.canDisplayType('audio/mp4', ' mhm1.0x0E');
